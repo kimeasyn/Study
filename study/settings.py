787db33ef7cf5a94e7330c18303526ff4eab8a78
kimeasyn <kimeasyn@gmail.com>
2017-11-23 17:49:06 +0900

    auth.models.User에 OneToOne필드로 accounts.models.Profile 모델 지정

--- study/settings.py
+++ study/settings.py
@@ -81,6 +81,9 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS':{
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
         'NAME': 'Study',
         'USER': 'heejin',
         'PASSWORD': 'gmlwls00',
