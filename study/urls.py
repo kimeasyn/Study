df8146b8a008ed975964655721915a967da15303
kimeasyn <kimeasyn@gmail.com>
2017-11-23 17:30:17 +0900

    accounts app 추가

--- study/urls.py
+++ study/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^study/', include('studyApp.urls'))
+    url(r'^study/', include('studyApp.urls')),
+    url(r'^accounts/', include('accounts.urls')),
 ]
