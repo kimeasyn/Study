7950dd3f6ac633b7cb47d90328e07b66fd04201c
kimeasyn <kimeasyn@gmail.com>
2017-12-03 16:10:27 +0900

    url namespace 사용

--- study/urls.py
+++ study/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^study/', include('studyApp.urls')),
-    url(r'^accounts/', include('accounts.urls')),
+    url(r'^study/', include('studyApp.urls', namespace='study')),
+    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
 ]
--- studyApp/urls.py
+++ studyApp/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.study_list),
+    url(r'^$', views.study_list, name='study_list'),
     url(r'^(?P<id>\d+)/$', views.study_detail, name='study_detail'),
 ]
